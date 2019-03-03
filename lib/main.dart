66b6f80aad5ac695c6198bf956dce85f72ef974f
Davidodari <davidkibzodari@gmail.com>
2019-03-03 23:11:30 +0300

    [#] Navigation to two screens and main page screen

--- lib/main.dart
+++ lib/main.dart
@@ -28,28 +28,98 @@ class _FiseHomePageState extends State<FiseHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Card(
+            margin: EdgeInsets.all(25.0),
+            shape: BeveledRectangleBorder(),
+            child: Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const ListTile(
+                    title: Text('Fise Survey',
+                        style: TextStyle(
+                          fontSize: 19,
+                        )),
+                    subtitle: Text(
+                        'Welcome to Fise Survey ,please spare 5-6 min of your time to answer some few questions',
+                        style: TextStyle(
+                          height: 2.5,
+                          fontSize: 16,
+                        )),
+                  ),
+                  ButtonTheme.bar(
+                    // make buttons use the appropriate styles for cards
+                    child: ButtonBar(
+                      children: <Widget>[
+                        FlatButton(
+                          child: const Text(
+                            'OK',
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => IncomeSurveyRoute()));
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+        ));
+  }
+}
+
+class IncomeSurveyRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Income'),
       ),
       body: Center(
-        child: Card(
-          margin:EdgeInsets.all(29.0),
-          shape: BeveledRectangleBorder(),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              const ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('Fise Survey'),
-                subtitle: Text(
-                    'Welcome to Fise Survey ,please spare 5-6 min of your time to answer some few questions'),
-              ),
-            ],
-          ),
+        child: RaisedButton(
+          child: Text('Next'),
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => HouseholdIncomeSurveyRoute()));
+          },
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class HouseholdIncomeSurveyRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Household Income"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: Text('Next'),
+        ),
+      ),
     );
   }
 }
