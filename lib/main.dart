d4c3bd310f231a8ac9c45c81b7b439a141b597c3
Davidodari <davidkibzodari@gmail.com>
2019-03-10 19:22:32 +0300

    [#] Remove Unused List

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  final List<int> surveyScoreCard = [];
 
   @override
   Widget build(BuildContext context) {
